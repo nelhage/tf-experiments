7d708c7e632b20ea3e1c4c5916de770b40841ef7
Nelson Elhage <nelhage@nelhage.com>
2017-02-24 15:15:06 -0800

    new ami

---
+++ cloud/scripts/tf.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+cd ~ubuntu
+virtualenv --python=python3 venv
+venv/bin/pip install --upgrade pip
+venv/bin/pip install tensorflow tensorflow-gpu
+venv/bin/pip install https://storage.googleapis.com/tensorflow/linux/cpu/protobuf-3.1.0-cp35-none-linux_x86_64.whl
