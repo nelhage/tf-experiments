2addb0c75b0e1835f59d4c7be31775b8d0fd5cf1
Nelson Elhage <nelhage@nelhage.com>
2017-02-26 15:00:30 -0500

    pass more flags to cuda install

--- cloud/scripts/cuda.sh
+++ cloud/scripts/cuda.sh
@@ -5,7 +5,7 @@ apt-get -y install libcupti-dev linux-image-extra-virtual
 cd /tmp/
 
 curl -LO https://developer.nvidia.com/compute/cuda/8.0/prod/local_installers/cuda_8.0.44_linux-run
-sh cuda_8.0.44_linux-run --silent --driver --toolkit
+sh cuda_8.0.44_linux-run --silent --driver --toolkit --toolkitpath=/usr/local/cuda --verbose
 
 curl -LO https://nelhage-ml.s3.amazonaws.com/sw/cudnn-8.0-linux-x64-v5.1.tgz
 tar -C /usr/local -xzf "cudnn-8.0-linux-x64-v5.1.tgz"
