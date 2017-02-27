242785f760e6b6e1a9c61a628e41f6002d42631b
Nelson Elhage <nelhage@nelhage.com>
2017-02-26 19:52:23 -0500

    fix better

--- cloud/scripts/base.sh
+++ cloud/scripts/base.sh
@@ -2,4 +2,6 @@
 set -eux
 
 apt-get update
+apt-get -y upgrade
 sudo apt-get -y install build-essential virtualenv cmake zlib1g-dev linux-tools-common linux-tools-generic
+shutdown -r now
--- cloud/scripts/cuda.sh
+++ cloud/scripts/cuda.sh
@@ -1,11 +1,27 @@
 #!/bin/bash
 set -eux
 
-apt-get -y install libcupti-dev linux-image-extra-virtual
+apt-get -y install libcupti-dev "linux-image-extra-$(uname -r)"
 cd /tmp/
 
-curl -LO https://developer.nvidia.com/compute/cuda/8.0/prod/local_installers/cuda_8.0.44_linux-run
-sh cuda_8.0.44_linux-run --silent --driver --toolkit --toolkitpath=/usr/local/cuda --verbose
+# Driver (g2.2xlarge)
+
+curl -LO http://us.download.nvidia.com/XFree86/Linux-x86_64/367.57/NVIDIA-Linux-x86_64-367.57.run
+sh NVIDIA-Linux-x86_64-367.57.run -a -q --ui=none
+
+# CUDA
+
+curl -LO https://developer.nvidia.com/compute/cuda/8.0/Prod2/local_installers/cuda_8.0.61_375.26_linux-run
+sh cuda_8.0.61_375.26_linux-run --silent --toolkit --toolkitpath=/usr/local/cuda-8.0 --verbose
+
+if ! grep -q 'CUDA Version 8' /usr/local/cuda-8.0/version.txt; then
+    echo "CUDA install failed!" >&2
+    exit 1
+fi
+
+ln -s cuda-8.0 /usr/local/cuda
+
+# cuDNN
 
 curl -LO https://nelhage-ml.s3.amazonaws.com/sw/cudnn-8.0-linux-x64-v5.1.tgz
 tar -C /usr/local -xzf "cudnn-8.0-linux-x64-v5.1.tgz"
@@ -15,4 +31,13 @@ export CUDA_HOME=/usr/local/cuda
 export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$CUDA_HOME/lib64:$CUDA_HOME/extras/CUPTI/lib64"
 EOF
 
+
+# Blacklist nouveau
+cat > /etc/modprobe.d/blacklist-nouveau.conf <<EOF
+blacklist nouveau
+blacklist lbm-nouveau
+options nouveau modeset=0
+alias nouveau off
+alias lbm-nouveau off
+EOF
 update-initramfs -k all -u
